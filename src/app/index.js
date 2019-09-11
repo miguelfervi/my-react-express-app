2a51f798246d9c953eb47ac6a4942dbef1eddea7
Miguel Fernandez <mfernandez@financialforce.com>
2019-09-11 11:23:07 +0200

    Add store git push
    ls

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,13 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 import { store } from './store';
 
-console.log(store.getState());
+// console.log(store.getState());
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { Main } from '../app/components/Main';
+
+ReactDOM.render(
+  <Main></Main>,
+  document.getElementById("app")
+)
